596e71210b3b9c98879426185b70d0e1fa951093
Patrick Quam <pquam@hotmail.com>
2016-10-26 21:44:17 -0700

    Create fracSeudo.java
    Seudo Code for fracCalc

---
+++ fracSeudo.java
@@ -0,0 +1,32 @@
+
+Addition {
+
+    seperate equation by spaces
+    check to see if denominator is the same
+    (if not) {
+    devide larger denominator by smaller denominator to get X
+    multiply smaller fraction by X
+    }
+    add numerators
+    if final numerator is larger than common denominator, devide numerator by denominator, add result to whole number
+    take the % of numerator over denominator and use the remainder as the new numerator
+    add whole numbers, including excess from fractions
+    add fraction part onto the end
+    return new fraction.
+}
+
+Subtraction {
+
+    seperate equation by spaces
+    check to see if denominator is the same
+    (if not) {
+    devide larger denominator by smaller denominator to get X
+    multiply smaller fraction by X
+    }
+    subtract numerator 1 from numerator 2
+    if final numerator is larger than common denominator, devide numerator by denominator, add result to whole numbers
+    take the % of numerator over denominator and use the remainder as the new numerator
+   add excess from fractions to whole numbers, subtract whole numbers
+    add fraction part onto the end
+    return new fraction.
+}
