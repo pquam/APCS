3908fd4b8e43542d2fdac4a4bae4ca0b509f8785
Patrick Quam <pquam@hotmail.com>
2016-11-10 08:45:38 -0800

    Update FracCalc.java

--- FracCalc.java
+++ FracCalc.java
@@ -9,7 +9,9 @@ public class FracCalc {
         // TODO: Read the input from the user and call produceAnswer with an equation
 		
 		//string input declaration
-    	String input = "5_3/4 + 6_5/8";
+    	Scanner console = new Scanner(System.in);
+    	System.out.println("What is your equation?");
+    	String input = console.nextLine();
     	@SuppressWarnings("resource")
     	//start a new scanner called parser
 		Scanner parser = new Scanner(input).useDelimiter(" ");
@@ -24,12 +26,12 @@ public class FracCalc {
     	
     	//close parser
     	parser.close();
-		System.out.println("Your equation: " + list + " ");
+    	//*****System.out.println("Your equation: " + list + " ");
 		
 //		
 		//String declaration for operand
 		String op = "";
-		System.out.print("Your operand(s): ");
+		//*****System.out.print("Your operand(s): ");
 		//for each element of the array
 		for (int i = 0; i < list.size(); i++) {
 			
@@ -38,41 +40,45 @@ public class FracCalc {
 				
 				//set op equal to the operand, and print it out
 				op = list.get(i);
-				System.out.print(op + "  ");
+				//*****System.out.print(op + "  ");
 			}
 		}
 		
-		System.out.println("List size: " + list.size());
+		//System.out.print(list.get(2) + " ");
+		//*****System.out.println("List size: " + list.size());
 		
 		//call method for whole numbers
     	int[] wns = new int[list.size() + 1];
 		WholeNumbers(list, wns);
+		System.out.print("whole:" + wns[2] + " ");
 		
 		//call method for numerator
     	int[] ns = new int[list.size() + 1];
 		numerator(list, ns);
+		System.out.print("numerator:" + ns[2] + " ");
 				
 		//call method to find denominator
 		int[] ds = new int[list.size() + 1];
 		denominator(list, ds);
+		System.out.print("denominator:" + ds[2] + " ");
 		
 		String answer = "";
 		
 		//call add method if the operand is add
 		if (op.equals("+")) {
 			
-			System.out.println("Addition");
-			System.out.println("Answer: " + add(list, wns, ns, ds));
+			//*****System.out.println("Addition");
+			//*****System.out.println("Answer: " + add(list, wns, ns, ds));
 		}
 		
 		//call subtract method if operand equals subtract
 		if (op.equals("-")) {
 			
-			System.out.println("subtraction");
+			//*****System.out.println("subtraction");
 			sub(list);
 		}
 		
-    	System.out.println(answer);
+		//*****System.out.println(answer);
 
     }
     
@@ -98,14 +104,14 @@ public class FracCalc {
     //method for finding whole numbers
     public static void WholeNumbers(ArrayList<String> list, int[] wns) {
     	
-    	System.out.println("");
+    	//*****System.out.println("");
 		//
 			//References list for pieces of equation
 	    	String numbers = "";
 	    	//temporary denominator of each piece of equation
 	    	String wn = "";
 	    	int number = 0;
-	    	System.out.print("Your Whole Number(s): ");
+	    	//*****System.out.print("Your Whole Number(s): ");
 	    	//for each item in ArrayList list
 	    	for (int i = 0; i < list.size(); i++) {
 	    		
@@ -127,14 +133,14 @@ public class FracCalc {
 	    	//
 	    	for (int i = 0; i <= wns.length - 2; i++) {
 	    		
-	    		System.out.print(wns[i] + " ");
+	    		//*****System.out.print(wns[i] + " ");
 	    	}
     }
     
     //method for finding numerator
     public static void numerator(ArrayList<String> list, int[] ns) {
     	
-    	System.out.println("");
+    	//*****System.out.println("");
 		//
     	
 			//References list for pieces of equation
@@ -145,7 +151,7 @@ public class FracCalc {
 	    	int in = 0;
 	    	//temporary denominator of each piece of equation
 	    	int n = 0;
-	    	System.out.print("Your numerator(s): ");
+	    	//*****System.out.print("Your numerator(s): ");
 	    	//for each item in ArrayList list
 	    	for (int i = 0; i < list.size(); i++) {
 	    		
@@ -167,14 +173,14 @@ public class FracCalc {
 	    	//
 	    	for (int i = 0; i <= ns.length - 2; i++) {
 	    		
-	    		System.out.print(ns[i] + " ");
+	    		//*****System.out.print(ns[i] + " ");
 	    	}
     }
     
     // method for finding denominator
     public static void denominator(ArrayList<String> list, int[] ds) {
     	
-		System.out.println("");
+		//*****System.out.println("");
 		//
 			//References list for pieces of equation
 	    	String numbers = "";
@@ -182,7 +188,7 @@ public class FracCalc {
 	    	int d = 0;
 	    	//array for denominators
 	    	
-	    	System.out.print("Your denominator(s): ");
+	    	//*****System.out.print("Your denominator(s): ");
 	    	//for each item in ArrayList list
 	    	for (int i = 0; i < list.size(); i++) {
 	    		
@@ -204,7 +210,7 @@ public class FracCalc {
 	    	//
 	    	for (int i = 0; i <= ds.length - 2; i++) {
 	    		
-	    		System.out.print(ds[i] + " ");
+	    		//*****System.out.print(ds[i] + " ");
 	    	}
 	    	return;
     }
@@ -220,7 +226,7 @@ public class FracCalc {
     		b = a;
     		a = b2;
     	}
-    	System.out.print("A: " + a + " ");
+    	//*****System.out.print("A: " + a + " ");
     	return Math.abs(a);
     	
     	
@@ -229,10 +235,10 @@ public class FracCalc {
     //method for addition
 	public static String add(ArrayList<String> list, int[] wns, int[] ns, int[] ds) {
     	
-		System.out.print("ds: " + ds[1] + "" + ds[3]);
+		//*****System.out.print("ds: " + ds[1] + "" + ds[3]);
 		int a = ds[1];
 		int b = ds[3];
-		System.out.print("D: " + gcd(a, b) + " ");
+		//*****System.out.print("D: " + gcd(a, b) + " ");
 		//int for common denominator
 		int D = 1;
 		//check to see if denominators match
